db30194ba5e8d5f3245bbe4f3701e089cb6ac9c1
dmoneyballer <drew.gilliesbc@gmail.com>
2022-08-10 21:17:36 -0600

    added working file

--- finder-app/finder.c
+++ finder-app/finder.c
@@ -7,8 +7,7 @@ int main(int argc, char ** argv) {
         printf("please call this with 2 args, <dir to search> <text to search> to run this %d command\n", argc);
         return 1;
     }
-    // printf("grep -r %s %s\n", argv[2], argv[1]);
-    // for (int i=0; i<argc; i++) {
+
     FILE *fp;
     char buffer[BUFFER];
     char cmd1[112] = "find ";
@@ -17,49 +16,50 @@ int main(int argc, char ** argv) {
     char *dir = argv[1];
     strcat(cmd1, dir);
     strcat(cmd1,cmd2);
-    // free(dir);
-    // char final = strcpy(cmd,/cmd2);
-    // char cmd = "find %s -type f|wc -l", argv[1];
-    // char cmd = printf("find %s -type f|wc -l\n", argv[1]);
+
     fp = popen(cmd1, "r");
     FILE * wf;
 
     int ch;
-    char str;
-    wf = fopen("/tmp/num", "w");
 
+    wf = fopen("/tmp/num", "w");
     while ((ch=fgetc(fp)) != EOF)
-        // printf("%d\n",ch);
-        // strcat(str,ch);
-        // printf("\n");
         putc(ch, wf);
-        // printf("\n");
-    // while (fgets(buffer, BUFFER-1, fp) != NULL) {
-    //     numb = buffer;
-    //     printf("output[%d]: %s", count, buffer);
-    //     count ++;
-    // }
+
     pclose(fp);
     pclose(wf);
     FILE *fp1;
+    FILE *wf1;
     char grep_cmd[112] = "grep -r ";
     char *search = argv[2];
     strcat(grep_cmd, search);
     strcat(grep_cmd, dir);
     strcat(grep_cmd, " 2>/dev/null|wc -l");
-    printf("%s", grep_cmd);
     fp1 = popen(grep_cmd, "r");
-    // int count =0;
-    // char num_found;
-    // while (fgets(buffer2, BUFFER-1, fp) != NULL) {
-    //     strcat(buffer2, num_found);
-    //     // printf("output[%d]: %s", count, buffer);
-    //     count ++;
-    // }
-    // pclose(fp);
-    // printf("The number of files are %s and the number of matching lines are %s", num_files, num_found);
+    int ch1;
+    wf1 = fopen("/tmp/occur", "w");
+    while ((ch=fgetc(fp1)) != EOF)
 
+        putc(ch, wf1);
 
-    // printf("\nnum is%d\n", str);
+    pclose(fp1);
+    pclose(wf1);
+
+    char x[999];
+    FILE* file_num = fopen("/tmp/num", "r");
+    while (fgets(x, 100, file_num) )
+    {
+    ;
+    }
+    pclose(file_num);
+    char y[999];
+    FILE* file_occur = fopen("/tmp/occur", "r");
+    while (fgets(y, 100, file_occur) )
+    {
+    ;
+    }
+    pclose(file_occur);
+    x[strlen(x)-1] = 0;
+    printf("The number of files are %s and the number of matching lines are %s", x, y);
     return 0;
 }
