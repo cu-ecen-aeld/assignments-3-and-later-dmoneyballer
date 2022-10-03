9ba22540f017427a3e5a39f27f3bada00553e316
dmoneyballer <drew.gilliesbc@gmail.com>
2022-10-03 17:25:50 -0600

    added9

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -86,10 +86,10 @@ sudo make -j ARCH=${ARCH} install
 
 # TODO: Add library dependencies to rootfs
 echo "Library dependencies"
-# ${CROSS_COMPILE}readelf -a /bin/busybox | grep "program interpreter"
-# ${CROSS_COMPILE}readelf -a /bin/busybox | grep "Shared library"
+ ${CROSS_COMPILE}readelf -a /bin/busybox | grep "program interpreter"
+ ${CROSS_COMPILE}readelf -a /bin/busybox | grep "Shared library"
 cd "$OUTDIR"
-# exit 0
+ exit 0
 # cp -a $OUTDIR/lib/ld-linux-armf.so.3 lib
 # cp -a $OUTDIR/lib/ld-2.22.so lib
 cp -a /home/dmoneyballa/Downloads/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/usr/lib64/libc.so lib
