3a5fcd050ff6d2ed5f3f0732bf8482cd502fbca7
dmoneyballer <drew.gilliesbc@gmail.com>
2022-10-04 14:23:54 -0600

    diff boot image

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -52,7 +52,7 @@ fi
 
 echo "Adding the Image in outdir"
 cd "$OUTDIR"
-cp  $OUTDIR/linux-stable/vmlinux $OUTDIR/Image
+cp  $OUTDIR/linux-stable/arch/arm64/boot/Image $OUTDIR/Image
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
