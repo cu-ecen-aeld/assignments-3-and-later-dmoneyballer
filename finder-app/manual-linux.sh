37cd889ab90a34916e5430a1647e4ba17d9c2280
dmoneyballer <drew.gilliesbc@gmail.com>
2022-10-04 15:39:50 -0600

    what

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -157,9 +157,9 @@ cd "$OUTDIR/rootfs"
 # mkir -p var/log
 # rm dev/*
 sudo mknod -m 666 dev/null c 1 3
-sudo mknod -m 666 dev/console c 5 1
-sudo mknod -m 666 dev/Console c 1 5
-sudo mknod -m 666 dev/tty c 1 5
+sudo mknod -m 600 dev/console c 5 1
+# sudo mknod -m 666 dev/Console c 1 5
+# sudo mknod -m 666 dev/tty c 1 5
 # # TODO: Clean and build the writer utility
 cd $FINDER_APP_DIR
 # make clean e modules_install step discus
