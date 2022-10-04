aa22d25d82bfa032d2796858e2367611dffc6693
dmoneyballer <drew.gilliesbc@gmail.com>
2022-10-04 11:26:41 -0600

    hope

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -93,9 +93,14 @@ echo "Library dependencies"
  cd "$OUTDIR"
 #  grep -ir \.so $OUTDIR
 ls -l $OUTDIR/lib
+sleep 5
 ls -l /
+sleep 5
 ls -l /home
+sleep 5
 ls -l /lib
+sleep 5
+ls -l /usr/lib
 # cp -a $OUTDIR/lib/ld-linux-armf.so.3 lib
 # cp -a $OUTDIR/lib/ld-2.22.so lib
 #cp -a /home/dmoneyballa/Downloads/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/usr/lib64/libc.so lib
