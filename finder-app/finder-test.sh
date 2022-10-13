c9a2482c9161f9d2cd5efa406adc8829a077d46c
dmoneyballer <drew.gilliesbc@gmail.com>
2022-10-13 15:03:35 -0600

    add diff commands

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -43,10 +43,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
